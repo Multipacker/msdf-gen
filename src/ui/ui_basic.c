68a7b7b104e3b78d8575a243600222053399e8d6
Simon Renhult <simon.renhult@hotmail.se>
2024-06-22 11:58:10 +0200

    Bugfix(ui): Push and pop UI rows and columns.

--- src/ui/ui_basic.c
+++ src/ui/ui_basic.c
@@ -10,14 +10,26 @@ internal UI_Box *ui_spacer_sized(UI_Context *ui, UI_Size size) {
     return spacer;
 }
 
-internal UI_Box *ui_row(UI_Context *ui) {
+internal UI_Box *ui_row_begin(UI_Context *ui) {
     ui_layout_axis_next(ui, Axis2_X);
     UI_Box *row = ui_box_create(ui, 0);
+    ui_parent_push(ui, row);
     return row;
 }
 
-internal UI_Box *ui_column(UI_Context *ui) {
+internal UI_Box *ui_row_end(UI_Context *ui) {
+    UI_Box *result = ui_parent_pop(ui);
+    return result;
+}
+
+internal UI_Box *ui_column_begin(UI_Context *ui) {
     ui_layout_axis_next(ui, Axis2_Y);
     UI_Box *column = ui_box_create(ui, 0);
+    ui_parent_push(ui, column);
     return column;
+}
+
+internal UI_Box *ui_column_end(UI_Context *ui) {
+    UI_Box *result = ui_parent_pop(ui);
+    return result;
 }
--- src/ui/ui_basic.h
+++ src/ui/ui_basic.h
@@ -3,7 +3,13 @@
 
 internal UI_Box *ui_spacer(UI_Context *ui);
 internal UI_Box *ui_spacer_sized(UI_Context *ui, UI_Size size);
-internal UI_Box *ui_row(UI_Context *ui);
-internal UI_Box *ui_column(UI_Context *ui);
+
+internal UI_Box *ui_row_begin(UI_Context *ui);
+internal UI_Box *ui_row_end(UI_Context *ui);
+#define ui_row(ui) defer_loop(ui_row_begin(ui), ui_row_end(ui))
+
+internal UI_Box *ui_column_begin(UI_Context *ui);
+internal UI_Box *ui_column_end(UI_Context *ui);
+#define ui_column(ui) defer_loop(ui_column_begin(ui), ui_column_end(ui))
 
 #endif //UI_BASIC_H
