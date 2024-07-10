0bfd15928d15593e82f8e88986bf094fdad22332
Simon Renhult <simon.renhult@hotmail.se>
2024-07-10 21:15:11 +0200

    Dev(base): Added more color conversion routines.

--- src/base/base_color.c
+++ src/base/base_color.c
@@ -1,4 +1,4 @@
-internal F32 f32_srgb_to_linear(F32 srgb) {
+internal F32 f32_linear_from_srgb(F32 srgb) {
     F32 linear = 0.0f;
 
     if (srgb <= 0.04045f) {
@@ -10,11 +10,25 @@ internal F32 f32_srgb_to_linear(F32 srgb) {
     return linear;
 }
 
+internal F32 f32_srgb_from_linear(F32 linear) {
+    F32 srgb = 0.0f;
+
+    if (linear <= 0.0031308f) {
+        srgb = 12.92f * linear;
+    } else {
+        srgb = 1.055f * f32_pow(linear, 1.0f / 2.4f) - 0.055f;
+    }
+
+    return srgb;
+}
+
+
+
 internal V4F32 color_from_srgba_u8(U8 red, U8 green, U8 blue, U8 alpha) {
     V4F32 result = v4f32(
-        f32_srgb_to_linear((F32) red   / 255.0f),
-        f32_srgb_to_linear((F32) green / 255.0f),
-        f32_srgb_to_linear((F32) blue  / 255.0f),
+        f32_linear_from_srgb((F32) red   / 255.0f),
+        f32_linear_from_srgb((F32) green / 255.0f),
+        f32_linear_from_srgb((F32) blue  / 255.0f),
         (F32) alpha / 255.0f
     );
 
@@ -30,4 +44,24 @@ internal V4F32 color_from_srgba_u32(U32 rgba) {
     );
 
     return result;
+}
+
+
+
+internal U32 u32_srgba_from_color(V4F32 color) {
+    V4F32 clamped = v4f32(
+        f32_clamp(color.r, 0.0f, 1.0f),
+        f32_clamp(color.g, 0.0f, 1.0f),
+        f32_clamp(color.b, 0.0f, 1.0f),
+        f32_clamp(color.a, 0.0f, 1.0f)
+    );
+
+    U8 red   = (U8) f32_round(f32_srgb_from_linear(clamped.r) * 255.0f);
+    U8 green = (U8) f32_round(f32_srgb_from_linear(clamped.g) * 255.0f);
+    U8 blue  = (U8) f32_round(f32_srgb_from_linear(clamped.b) * 255.0f);
+    U8 alpha = (U8) f32_round(clamped.a * 255.0f);
+
+    U32 srgba = red << 24 | blue << 16 | green << 8 | alpha;
+
+    return srgba;
 }
--- src/base/base_color.h
+++ src/base/base_color.h
@@ -1,9 +1,12 @@
 #ifndef BASE_COLOR_H
 #define BASE_COLOR_H
 
-internal F32 f32_srgb_to_linear(F32 srgb);
+internal F32 f32_linear_from_srgb(F32 srgb);
+internal F32 f32_srgb_from_linear(F32 srgb);
 
 internal V4F32 color_from_srgba_u8(U8 red, U8 green, U8 blue, U8 alpha);
 internal V4F32 color_from_srgba_u32(U32 rgba);
+
+internal U32 u32_srgba_from_color(V4F32 color);
 
 #endif // BASE_COLOR_H
