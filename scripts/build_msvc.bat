e05481d19492e17feed56f855d968616fade4f7c
Simon Renhult <simon.renhult@hotmail.se>
2024-08-19 13:56:49 +0200

    Dev(scripts): Don't error on undefined macros on Windows.

--- scripts/build_msvc.bat
+++ scripts/build_msvc.bat
@@ -11,12 +11,13 @@ rem 4820: n bytes padding added after data member x
 rem 5045: Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
 rem 4711: x selected for automatic inline expansion
 rem 4710: function not inlined
-rem 4242: 4242 conversion from U32 to U16
-rem 4244: 4244 conversion from U32 to U8
+rem 4242: conversion from U32 to U16
+rem 4244: conversion from U32 to U8
+rem 4668: undefined preprocessor macros
 
 rem -- Common flags --
 
-set disabled_warnings=-wd4201 -wd4152 -wd4100 -wd4189 -wd4101 -wd4310 -wd4061 -wd4820 -wd4191 -wd5045 -wd4711 -wd4710 -wd4242 -wd4244
+set disabled_warnings=-wd4201 -wd4152 -wd4100 -wd4189 -wd4101 -wd4310 -wd4061 -wd4820 -wd4191 -wd5045 -wd4711 -wd4710 -wd4242 -wd4244 -wd4668
 set compiler_flags=-nologo -FC -Wall -MP -WX %disabled_warnings% -I..
 set libs=User32.lib Opengl32.lib Gdi32.lib
 set linker_flags=%libs% -incremental:no
