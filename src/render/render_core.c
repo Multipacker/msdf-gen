738faf230ed02208184a1d30745ee4816fb572bc
Simon Renhult <simon.renhult@hotmail.se>
2025-01-23 02:16:20 +0100

    Opt(render): Allocate smaller shape chunks and only zero out shapes on demand
    This significantly improves performance, resulting in us drawing the
    whole UI between 2 and 6 times faster. We also saved a lot of unused
    memory: the UI currently only renders about 300 rectangles, but we
    generate 15 batches. Each bucket you can push shapes into was 512 in
    size. Meaning all of our shapes could have fit in the memory for a
    single batch, but couldn't due to differeing draw parameters.

--- src/render/render_core.c
+++ src/render/render_core.c
@@ -2,14 +2,15 @@ internal Render_Shape *render_shape_list_push(Arena *arena, Render_ShapeList *sh
     Render_ShapeChunk *chunk = shapes->last;
 
     if (!chunk || chunk->count == chunk->capacity) {
-        chunk = arena_push_struct_zero(arena,  Render_ShapeChunk);
-        chunk->capacity = 512;
-        chunk->shapes = arena_push_array_zero(arena, Render_Shape, chunk->capacity);
+        chunk = arena_push_struct_zero(arena, Render_ShapeChunk);
+        chunk->capacity = 32;
+        chunk->shapes = arena_push_array(arena, Render_Shape, chunk->capacity);
         sll_queue_push(shapes->first, shapes->last, chunk);
         ++shapes->chunk_count;
     }
 
     Render_Shape *result = &chunk->shapes[chunk->count];
+    memory_zero_struct(result);
     ++chunk->count;
     ++shapes->shape_count;
 
