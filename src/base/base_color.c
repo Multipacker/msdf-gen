6be1c9fc8ef4bf8d466ff631860ae9632ce94da1
Simon Renhult <simon.renhult@hotmail.se>
2025-01-26 21:05:36 +0100

    Dev(base/color): Added functions for converting from and to HSVA

--- src/base/base_color.c
+++ src/base/base_color.c
@@ -24,6 +24,16 @@ internal F32 f32_srgb_from_linear(F32 linear) {
 
 
 
+internal V4F32 color_from_srgba(V4F32 srgba) {
+    V4F32 result = v4f32(
+        f32_linear_from_srgb(srgba.r),
+        f32_linear_from_srgb(srgba.g),
+        f32_linear_from_srgb(srgba.b),
+        srgba.a
+    );
+    return result;
+}
+
 internal V4F32 color_from_srgba_u8(U8 red, U8 green, U8 blue, U8 alpha) {
     V4F32 result = v4f32(
         f32_linear_from_srgb((F32) red   / 255.0f),
@@ -48,6 +58,16 @@ internal V4F32 color_from_srgba_u32(U32 rgba) {
 
 
 
+internal V4F32 srgba_from_color(V4F32 color) {
+    V4F32 result = v4f32(
+        f32_srgb_from_linear(color.r),
+        f32_srgb_from_linear(color.g),
+        f32_srgb_from_linear(color.b),
+        color.a
+    );
+    return result;
+}
+
 internal U32 u32_srgba_from_color(V4F32 color) {
     V4F32 clamped = v4f32(
         f32_clamp(color.r, 0.0f, 1.0f),
@@ -64,4 +84,72 @@ internal U32 u32_srgba_from_color(V4F32 color) {
     U32 srgba = red << 24 | blue << 16 | green << 8 | alpha;
 
     return srgba;
+}
+
+
+
+internal V4F32 srgba_from_hsva(V4F32 hsva) {
+    F32 hue        = hsva.x;
+    F32 saturation = hsva.y;
+    F32 value      = hsva.z;
+    F32 alpha      = hsva.w;
+
+    F32 chroma    = value * saturation;
+    F32 hue_prime = hue / 60.0f;
+    F32 x         = chroma * (1.0f - f32_abs(f32_mod(hue_prime, 2.0f) - 1.0f));
+
+    S32 sector = s32_min(s32_max(0, (S32) f32_floor(hue_prime)), 5);
+    F32 reds[]   = { chroma, x,      0.0f,   0.0f,   x,      chroma, };
+    F32 greens[] = { x,      chroma, chroma, x,      0.0f,   0.0f,   };
+    F32 blues[]  = { 0.0f,   0.0f,   x,      chroma, chroma, x,      };
+
+    F32 m = value - chroma;
+
+    V4F32 result = v4f32(
+        m + reds[sector],
+        m + greens[sector],
+        m + blues[sector],
+        alpha
+    );
+    return result;
+}
+
+internal V4F32 hsva_from_srgba(V4F32 srgba) {
+    F32 red   = srgba.r;
+    F32 green = srgba.g;
+    F32 blue  = srgba.b;
+    F32 alpha = srgba.a;
+
+    // NOTE(simon): This chosen to smaller than 1 / 256 with the reasoning that
+    // humans have a hard time differentiating between colors that are closer
+    // together than by that amount. The reason we need an epsilon at all is
+    // because we otherwise would run into division by zero giving us either
+    // NaN or inifity.
+    F32 epsilon = 1.0f / 512.0f;
+
+    F32 min = f32_min(f32_min(red, green), blue);
+    F32 max = f32_max(f32_max(red, green), blue);
+
+    F32 chroma     = max - min;
+    F32 value      = max;
+    F32 saturation = value < epsilon ? 0.0f : (chroma / value);
+
+    F32 hue = 60.0f;
+    if (chroma < epsilon) {
+        hue *= 0.0f;
+    } else if (value == red) {
+        hue *= f32_mod(6.0f + (green - blue) / chroma, 6.0f);
+    } else if (value == green) {
+        hue *= 2.0f + (blue - red)   / chroma;
+    } else if (value == blue) {
+        hue *= 4.0f + (red  - green) / chroma;
+    }
+
+    V4F32 result = v4f32(
+        hue,
+        saturation,
+        value,
+        alpha
+    );
+    return result;
 }
--- src/base/base_color.h
+++ src/base/base_color.h
@@ -1,12 +1,33 @@
 #ifndef BASE_COLOR_H
 #define BASE_COLOR_H
 
+/*
+ * NOTE(simon):
+ * Color is assumed to have all components be in [0, 1]
+ *
+ * sRGBA is assumed to have all components be in [0, 1]
+ *
+ * HSVa is assumed to have its components in the following ranges
+ *   Hue        [0, 360)
+ *   Saturation [0, 1]
+ *   Value      [0, 1]
+ *   Alpha      [0, 1]
+ *
+ * When a color is represented as a U32, red occupies the highest 8 bits, then
+ * green, blue, and alpha, in that order.
+ */
+
 internal F32 f32_linear_from_srgb(F32 srgb);
 internal F32 f32_srgb_from_linear(F32 srgb);
 
+internal V4F32 color_from_srgba(V4F32 srgba);
 internal V4F32 color_from_srgba_u8(U8 red, U8 green, U8 blue, U8 alpha);
 internal V4F32 color_from_srgba_u32(U32 rgba);
 
-internal U32 u32_srgba_from_color(V4F32 color);
+internal V4F32 srgba_from_color(V4F32 color);
+internal U32   u32_srgba_from_color(V4F32 color);
+
+internal V4F32 srgba_from_hsva(V4F32 hsva);
+internal V4F32 hsva_from_srgba(V4F32 srgba);
 
 #endif // BASE_COLOR_H
