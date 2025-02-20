bac940089fc6f30470896a4b7d38cc83b1ec3763
Simon Renhult <simon.renhult@hotmail.se>
2025-02-20 13:12:55 +0100

    Dev(graphics): Removed SDL backend

--- src/render/opengl/sdl_opengl.c
+++
@@ -1,8 +0,0 @@
-internal B32 render_init(Void) {
-    return true;
-}
-
-internal Void opengl_backend_init(Void) {
-    // TODO: Figure out how to move SDLs logic for initializing OpenGL into
-    // here.
-}
