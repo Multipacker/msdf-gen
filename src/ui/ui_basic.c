f4e5e31a1bff6bf1c7a87e937e96f544f3a9a472
Simon Renhult <simon.renhult@hotmail.se>
2025-02-24 21:33:00 +0100

    Dev(ui/basic): Added more navigation to line edits

--- src/ui/ui_basic.c
+++ src/ui/ui_basic.c
@@ -275,6 +275,11 @@ internal UI_Input ui_line_edit(U8 *buffer, U64 *buffer_size, U64 buffer_capacity
                     cursor_delta = (S64) (ptr - start) - (S64) *cursor;
                 } break;
                 case UI_EventDeltaUnit_Line: {
+                    if (event->delta.x < 0) {
+                        cursor_delta = -(S64) *cursor;
+                    } else {
+                        cursor_delta = (S64) edit_string.size - (S64) *cursor;
+                    }
                 } break;
                 case UI_EventDeltaUnit_Page: {
                 } break;
