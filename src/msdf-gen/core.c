f0a6ade93b27380e68d8fcd71fc7ca828cf411af
Simon Renhult <simon.renhult@hotmail.se>
2025-03-05 13:32:13 +0100

    Dev(msdf-gen): Clean up the command lister

--- src/msdf-gen/core.c
+++ src/msdf-gen/core.c
@@ -1034,34 +1034,63 @@ internal Void update(Void) {
         // NOTE(simon): Build command lister
         if (state->show_command_lister) {
             Arena_Temporary scratch = arena_get_scratch(0, 0);
-            F32 command_rectangle_width = (F32) client_area.width * 0.6f;
+
+            local U8 buffer[1024];
+            local U64 buffer_size = 0;
+            local U64 cursor = 0;
+            local U64 mark = 0;
+            local S64 active_index = 0;
+            local UI_ScrollPosition position = { 0 };
+
+            typedef struct CommandItem CommandItem;
+            struct CommandItem {
+                CommandKind command;
+                Str8 name;
+                FuzzyMatchList fuzzy_matches;
+            };
+            CommandItem commands[Command_COUNT] = { 0 };
+            U64 command_count = 0;
+
+            {
+                // NOTE(simon): Fill commands
+                for (CommandKind command = 0; command < array_count(commands); ++command) {
+                    commands[command_count].command       = command;
+                    commands[command_count].name          = command_names[command];
+                    commands[command_count].fuzzy_matches = str8_fuzzy_match(scratch.arena, str8(buffer, buffer_size), command_names[command]);
+                    ++command_count;
+                }
+
+                // NOTE(simon): Filter on number of matched parts
+                for (U64 i = 0; i < command_count;) {
+                    FuzzyMatchList matches = commands[i].fuzzy_matches;
+                    if (matches.needle_parts != matches.count) {
+                        swap(commands[i], commands[command_count - 1], CommandItem);
+                        --command_count;
+                    } else {
+                        ++i;
+                    }
+                }
+            }
+
+            F32 command_rectangle_width  = (F32) client_area.width * 0.6f;
             F32 command_rectangle_height = (F32) client_area.height * 0.8f;
-            ui_fixed_x_next(((F32) client_area.width - command_rectangle_width) / 2.0f);
+
+            ui_fixed_x_next(((F32) client_area.width  - command_rectangle_width)  / 2.0f);
             ui_fixed_y_next(((F32) client_area.height - command_rectangle_height) / 2.0f);
             ui_width_next(ui_size_pixels(command_rectangle_width, 1.0f));
             ui_height_next(ui_size_pixels(command_rectangle_height, 1.0f));
-            ui_layout_axis_next(Axis2_X);
+            ui_layout_axis_next(Axis2_Y);
             ui_focus_next(UI_Focus_Root);
             UI_Box *command_box = ui_create_box_from_string(
-                UI_BoxFlag_DrawBackground | UI_BoxFlag_DrawBorder | UI_BoxFlag_DrawDropShadow | UI_BoxFlag_Clickable | UI_BoxFlag_Scrollable,
+                UI_BoxFlag_DrawBackground | UI_BoxFlag_DrawBorder | UI_BoxFlag_DrawDropShadow |
+                UI_BoxFlag_Clickable | UI_BoxFlag_Scrollable,
                 str8_literal("##command_lister")
             );
 
-            ui_width(ui_size_fill())
-            ui_height(ui_size_fill())
             ui_parent(command_box)
-            ui_padding(ui_size_ems(0.5f, 1.0f))
-            ui_column()
-            ui_padding(ui_size_ems(0.5f, 1.0f))
+            ui_width(ui_size_fill())
             ui_height(ui_size_ems(1.5f, 1.0f))
-            ui_text_padding(ui_size_ems(0.5f, 1.0f).value)
-            {
-                local U8 buffer[1024];
-                local U64 buffer_size = 0;
-                local U64 cursor = 0;
-                local U64 mark = 0;
-                local S32 active_index = 0;
-
+            ui_text_padding(ui_size_ems(0.5f, 1.0f).value) {
                 UI_Key key = ui_key_from_string(ui_active_seed_key(), str8_literal("##query"));
                 ui_palette(palette_from_code(PaletteCode_Button))
                 ui_focus(UI_Focus_Active) {
@@ -1070,40 +1099,8 @@ internal Void update(Void) {
 
                 ui_spacer_sized(ui_size_ems(0.5f, 1.0f));
 
-                typedef struct CommandItem CommandItem;
-                struct CommandItem {
-                    CommandKind command;
-                    Str8 name;
-                    FuzzyMatchList fuzzy_matches;
-                };
-                CommandItem commands[Command_COUNT] = { 0 };
-                U64 command_count = array_count(commands);
-
-                {
-                    // NOTE(simon): Fill commands
-                    for (CommandKind command = 0; command < command_count; ++command) {
-                        commands[command].command       = command;
-                        commands[command].name          = command_names[command];
-                        commands[command].fuzzy_matches = str8_fuzzy_match(scratch.arena, str8(buffer, buffer_size), command_names[command]);
-                    }
-
-                    // NOTE(simon): Filter on number of matched parts
-                    for (U64 i = 0; i < command_count;) {
-                        FuzzyMatchList matches = commands[i].fuzzy_matches;
-                        if (matches.needle_parts != matches.count) {
-                            swap(commands[i], commands[command_count - 1], CommandItem);
-                            --command_count;
-                        } else {
-                            ++i;
-                        }
-                    }
-                }
-
-                local F32 scroll_offset = 0.0f;
-
                 // NOTE(simon): Scroll region
                 // TODO(simon): Replace with fixed size.
-                ui_width_next(ui_size_fill());
                 ui_height_next(ui_size_fill());
                 ui_layout_axis_next(Axis2_X);
                 UI_Box *region = ui_create_box_from_string(UI_BoxFlag_OverflowY | UI_BoxFlag_Clip | UI_BoxFlag_Scrollable, str8_literal("##region"));
@@ -1114,79 +1111,30 @@ internal Void update(Void) {
                 F32 scrollbar_width = (F32) ui_font_size_top();
                 F32 container_width = region_size.width - scrollbar_width;
                 F32 container_height = region_size.height;
+
                 F32 height = ui_size_ems(3.0, 1.0f).value;
+
+                // NOTE(simon): Properties of the data begin viewed.
+                S64 first_row    = 0;
+                S64 last_row     = (S64) command_count;
+                S64 visible_rows = (S64) f32_ceil(container_height / height);
+
+                // NOTE(simon): Properties of the current view.
+                S64 top_row    = position.index + (S64) f32_floor(position.offset);
+                S64 bottom_row = s64_min(top_row + (position.offset != 0.0f) + visible_rows, last_row);
 
                 // NOTE(simon): Scroll container
                 ui_width_next(ui_size_pixels(container_width, 1.0f));
                 ui_height_next(ui_size_pixels(container_height, 1.0f));
                 ui_layout_axis_next(Axis2_Y);
                 UI_Box *container = ui_create_box_from_string(0, str8_literal("##commands"));
-
-                S32 last_row = (S32) command_count;
-
-                local S32 scroll_row = 0;
-                S32 target_row = scroll_row;
-
-                S32 top_row    = scroll_row + (S32) f32_floor(scroll_offset);
-                S32 bottom_row = s32_min(top_row + (scroll_offset != 0.0f) + (S32) f32_ceil(region_size.height / height) - 1, last_row - 1);
-                container->view_offset.y = height * (f32_mod(scroll_offset, 1.0f) + (scroll_offset < 0.0f));
-
-                // NOTE(simon): Scrollbar container
-                ui_width_next(ui_size_pixels(scrollbar_width, 1.0f));
-                ui_height_next(ui_size_pixels(region_size.height, 1.0f));
-                ui_layout_axis_next(Axis2_Y);
-                UI_Box *scroll_container = ui_create_box_from_string(UI_BoxFlag_DrawBorder, str8_literal("##scrollbar"));
-
-                ui_width(ui_size_parent_percent(1.0f, 1.0f))
-                ui_palette(palette_from_code(PaletteCode_Button))
-                ui_parent(scroll_container) {
-                    F32 rows_above   = (F32) (scroll_row) + scroll_offset;
-                    F32 visible_rows = container_height / height;
-                    F32 row_count    = (F32) last_row + visible_rows - 1;
-                    F32 rows_below   = (F32) last_row - (F32) scroll_row - scroll_offset - 1;
-
-                    ui_hover_cursor_next(Gfx_Cursor_Hand);
-                    ui_height_next(ui_size_parent_percent(rows_above / row_count, 0.0f));
-                    UI_Box *scroll_before = ui_create_box_from_string(UI_BoxFlag_Clickable, str8_literal("##before"));
-
-                    ui_hover_cursor_next(Gfx_Cursor_Hand);
-                    ui_height_next(ui_size_parent_percent(f32_max(0.01f, visible_rows / row_count), 1.0f));
-                    UI_Box *scroll = ui_create_box_from_string(UI_BoxFlag_Clickable | UI_BoxFlag_DrawBackground | UI_BoxFlag_DrawBorder | UI_BoxFlag_DrawHot | UI_BoxFlag_DrawActive, str8_literal("##scroll"));
-
-                    ui_hover_cursor_next(Gfx_Cursor_Hand);
-                    ui_height_next(ui_size_parent_percent(rows_below / row_count, 0.0f));
-                    UI_Box *scroll_after = ui_create_box_from_string(UI_BoxFlag_Clickable, str8_literal("##after"));
-
-                    UI_Input before_input = ui_input_from_box(scroll_before);
-                    if (before_input.input_flags & UI_InputFlag_LeftClicked) {
-                        target_row -= (S32) f32_floor(visible_rows);
-                    }
-
-                    UI_Input scroll_input = ui_input_from_box(scroll);
-                    if (scroll_input.input_flags & UI_InputFlag_LeftDragging) {
-                        if (scroll_input.input_flags & UI_InputFlag_LeftPressed) {
-                            ui_set_drag_data(&top_row);
-                        }
-
-                        S32 start_row = *ui_get_drag_data(S32);
-
-                        F32 scroll_size = region_size.height - scroll->calculated_size.height;
-                        F32 drag_percent = ui_drag_delta().y / scroll_size;
-                        target_row = start_row + (S32) f32_floor(drag_percent * (row_count - visible_rows));
-                    }
-
-                    UI_Input after_input  = ui_input_from_box(scroll_after);
-                    if (after_input.input_flags & UI_InputFlag_LeftClicked) {
-                        target_row += (S32) f32_floor(visible_rows);
-                    }
-                }
+                container->view_offset.y = height * (f32_mod(position.offset, 1.0f) + (position.offset < 0.0f));
 
                 ui_parent(container)
                 ui_palette(palette_from_code(PaletteCode_Button))
-                ui_palette(palette_from_code(PaletteCode_Button))
                 ui_width(ui_size_fill())
                 ui_height(ui_size_pixels(height, 1.0f)) {
-                    for (S32 i = top_row; i <= bottom_row; ++i) {
+                    for (S64 i = top_row; i < bottom_row; ++i) {
                         if (i == active_index) {
                             ui_focus_next(UI_Focus_Active);
                         }
@@ -1204,6 +1152,7 @@ internal Void update(Void) {
                         if (command_button_input.input_flags & UI_InputFlag_Clicked) {
                             push_command(commands[i].command);
                             state->show_command_lister = 0;
+                            buffer_size = 0;
                         }
                     }
 
@@ -1212,63 +1161,61 @@ internal Void update(Void) {
                             continue;
                         }
 
-                        S32 delta = 0;
+                        S64 delta = 0;
                         switch (event->unit) {
                             case UI_EventDeltaUnit_Null: {
                             } break;
                             case UI_EventDeltaUnit_Character: {
-                                if (event->delta.y == -1) {
-                                    delta = -1;
-                                } else if (event->delta.y == 1) {
-                                    delta = 1;
-                                }
+                                delta += event->delta.y;
                             } break;
                             case UI_EventDeltaUnit_Word: {
                             } break;
                             case UI_EventDeltaUnit_Line: {
                             } break;
                             case UI_EventDeltaUnit_Page: {
-                                S32 rows_per_page = (S32) f32_ceil(container_height / height);
-                                if (event->delta.y == -1) {
-                                    delta = -rows_per_page;
-                                } else if (event->delta.y == 1) {
-                                    delta = rows_per_page;
-                                }
+                                delta += event->delta.y * visible_rows;
                             } break;
                             case UI_EventDeltaUnit_Whole: {
-                                if (event->delta.x == -1) {
-                                    delta = -active_index;
-                                } else if (event->delta.x == 1) {
-                                    delta = last_row - active_index - 1;
-                                }
                             } break;
                             case UI_EventDeltaUnit_COUNT: {
                             } break;
                         }
 
-                        active_index = s32_min(s32_max(0, active_index + delta), (S32) command_count - 1);
+                        active_index = s64_min(s64_max(0, active_index + delta), (S64) command_count);
 
                         ui_consume_event(event);
                     }
 
-                    active_index = s32_min(s32_max(0, active_index), (S32) command_count - 1);
+                    active_index = s64_min(s64_max(0, active_index), (S64) command_count);
+                }
+
+                ui_palette(palette_from_code(PaletteCode_Button))
+                ui_width(ui_size_pixels(scrollbar_width, 1.0f))
+                ui_height(ui_size_pixels(region_size.height, 1.0f)) {
+                    position = ui_scroll_bar(position, 0, last_row, visible_rows);
                 }
 
                 // NOTE(simon): Region
                 ui_parent_pop();
 
-                UI_Input region_input = ui_input_from_box(region);
-                target_row -= (S32) region_input.scroll.y;
-
                 // NOTE(simon): Scrolling
-                target_row = s32_min(s32_max(0, target_row), s32_max(0, last_row - 1));
-                scroll_offset += (F32) scroll_row - (F32) target_row;
-                scroll_row = target_row;
+                UI_Input region_input = ui_input_from_box(region);
+                position.index  -= (S64) region_input.scroll.y;
+                position.offset += region_input.scroll.y;
+
+                // NOTE(simon): Clamp scrolling.
+                if (position.index < 0) {
+                    position.offset += (F32) position.index;
+                    position.index = 0;
+                } else if (last_row <= position.index) {
+                    position.offset -= (F32) (s64_max(0, last_row - 1) - position.index);
+                    position.index = s64_max(0, last_row - 1);
+                }
 
                 // NOTE(simon): Animation
-                scroll_offset += -scroll_offset * ui_animation_slow_rate();
-                if (f32_abs(scroll_offset) < 0.001f) {
-                    scroll_offset = 0.0f;
+                position.offset += -position.offset * ui_animation_slow_rate();
+                if (f32_abs(position.offset) < 0.001f) {
+                    position.offset = 0.0f;
                 } else {
                     request_frame();
                 }
@@ -1278,6 +1225,7 @@ internal Void update(Void) {
 
             if (ui_consume_event_kind(UI_EventKind_Cancel)) {
                 state->show_command_lister = 0;
+                buffer_size = 0;
             }
 
             // NOTE(simon): Close lister if you click outside of the dialog.
@@ -1290,6 +1238,7 @@ internal Void update(Void) {
                     )
                 ) {
                     state->show_command_lister = 0;
+                    buffer_size = 0;
                 }
             }
 
