17c203625994e9c008b5162d40080a697014bfae
Simon Renhult <simon.renhult@hotmail.se>
2025-06-03 07:40:45 +0200

    Dev(meta): Changed names of constants to better fir the codebase

--- src/meta/main.c
+++ src/meta/main.c
@@ -26,21 +26,37 @@ typedef enum {
     Elf_HeaderIdentification_COUNT = 16,
 } Elf_HeaderIdentification;
 
-// NOTE(simon): ELFCLASS64
-#define ELF_HEADER_CLASS_64  2
-// NOTE(simon): ELFDATA2LSB
-#define ELF_HEADER_DATA_2LSB 1
-// NOTE(simon): EFLOSABI_NONE
-#define ELF_OS_ABI_NONE      0
-// NOTE(simon): EFLOSABI_GNU / ELFOSABI_LINUX
-#define ELF_OS_ABI_LINUX     3
+typedef enum {
+    // NOTE(simon): ELFCLASS64
+    Elf_HeaderClass_64 = 2,
+} Elf_HeaderClass;
 
-// NOTE(simon): ET_REL
-#define ELF_HEADER_TYPE_RELOCATABLE 1
-// NOTE(simon): EM_X86_64
-#define ELF_HEADER_MACHINE_X86_64   62
-// NOTE(simon): EV_CURRENT
-#define ELF_HEADER_VERSION_CURRENT  1
+typedef enum {
+    // NOTE(simon): ELFDATA2LSB
+    Elf_HeaderData_2Lsb = 1,
+} Elf_HeaderData;
+
+typedef enum {
+    // NOTE(simon): EFLOSABI_NONE
+    Elf_OsAbi_None = 0,
+    // NOTE(simon): EFLOSABI_GNU / ELFOSABI_LINUX
+    Elf_OsAbi_Linux = 3,
+} Elf_OsAbi;
+
+typedef enum {
+    // NOTE(simon): ET_REL
+    Elf_HeaderType_Relocatable = 1,
+} Elf_HeaderType;
+
+typedef enum {
+    // NOTE(simon): EM_X86_64
+    Elf_HeaderMachine_X86_64 = 62,
+} Elf_HeaderMachine;
+
+typedef enum {
+    // NOTE(simon): EV_CURRENT
+    Elf_HeaderVersion_Current = 1,
+} Elf_HeaderVersion;
 
 // NOTE(simon): Elf64_Ehdr
 typedef struct Elf64_Header Elf64_Header;
@@ -61,18 +77,23 @@ struct Elf64_Header {
     U16        section_header_string_index;                    // e_shstrndx
 };
 
-// NOTE(simon): SHT_NULL
-#define ELF_SECTION_HEADER_TYPE_NULL         0
-// NOTE(simon): SHT_PROGBITS
-#define ELF_SECTION_HEADER_TYPE_PROGRAM_BITS 1
-// NOTE(simon): SHT_SYMTAB
-#define ELF_SECTION_HEADER_TYPE_SYMBOL_TABLE 2
-// NOTE(simon): SHT_STRTAB
-#define ELF_SECTION_HEADER_TYPE_STRING_TABLE 3
-// NOTE(simon): SHF_WRITE
-#define ELF_SECTION_HEADER_FLAG_WRITE        0x01
-// NOTE(simon): SHF_ALLOC
-#define ELF_SECTION_HEADER_FLAG_ALLOCATE     0x02
+typedef enum {
+    // NOTE(simon): SHT_NULL
+    Elf_SectionHeaderType_Null        = 0,
+    // NOTE(simon): SHT_PROGBITS
+    Elf_SectionHeaderType_ProgramBits = 1,
+    // NOTE(simon): SHT_SYMTAB
+    Elf_SectionHeaderType_SymbolTable = 2,
+    // NOTE(simon): SHT_STRTAB
+    Elf_SectionHeaderType_StringTable = 3,
+} Elf_SectionHeaderType;
+
+typedef enum {
+    // NOTE(simon): SHF_WRITE
+    Elf_SectionHeaderFlag_Write    = 1 << 0,
+    // NOTE(simon): SHF_ALLOC
+    Elf_SectionHeaderFlag_Allocate = 1 << 1,
+} Elf_SectionHeaderFlags;
 
 // NOTE(simon): Elf64_Shdr
 typedef struct Elf64_SectionHeader Elf64_SectionHeader;
@@ -89,14 +110,22 @@ struct Elf64_SectionHeader {
     U64        entry_size;    // sh_entsize
 };
 
-// NOTE(simon): STT_OBJECT
-#define ELF_SYMBOL_TYPE_OBJECT        1
-// NOTE(simon): STB_LOCAL
-#define ELF_SYMBOL_BINDING_LOCAL      0
-// NOTE(simon): STB_GLOBAL
-#define ELF_SYMBOL_BINDING_GLOBAL     1
-// NOTE(simon): STV_DEFAULT
-#define ELF_SYMBOL_VISIBILITY_DEFAULT 0
+typedef enum {
+    // NOTE(simon): STT_OBJECT
+    Elf_SymbolType_Object = 1,
+} Elf_SymbolType;
+
+typedef enum {
+    // NOTE(simon): STB_LOCAL
+    Elf_SymbolBinding_Local  = 0,
+    // NOTE(simon): STB_GLOBAL
+    Elf_SymbolBinding_Global = 1,
+} Elf_SymbolBinding;
+
+typedef enum {
+    // NOTE(simon): STV_DEFAULT
+    Elf_SymbolVisibility_Default = 0,
+} Elf_SymbolVisibility;
 
 #define ELF_SYMBOL_INFO_FROM_BINDING_TYPE(binding, type) (((binding) & 0x0F) << 4 | ((type) & 0x0F) << 0)
 
@@ -113,10 +142,10 @@ struct Elf64_Symbol {
 
 
 
-typedef struct Symbol Symbol;
-struct Symbol {
-    Symbol *next;
-    Symbol *previous;
+typedef struct Elf_Symbol Elf_Symbol;
+struct Elf_Symbol {
+    Elf_Symbol *next;
+    Elf_Symbol *previous;
 
     // NOTE(simon): Specified data.
     Str8 name;
@@ -129,10 +158,10 @@ struct Symbol {
     U64 offset;
 };
 
-typedef struct Section Section;
-struct Section {
-    Section *next;
-    Section *previous;
+typedef struct Elf_Section Elf_Section;
+struct Elf_Section {
+    Elf_Section *next;
+    Elf_Section *previous;
 
     // NOTE(simon): Specified data.
     Str8     name;
@@ -154,42 +183,43 @@ struct Section {
     U64 offset;
 };
 
-typedef struct Builder Builder;
-struct Builder {
-    Symbol *first_symbol;
-    Symbol *last_symbol;
+typedef struct Elf_Object Elf_Object;
+struct Elf_Object {
+    Elf_Symbol *first_symbol;
+    Elf_Symbol *last_symbol;
     U64 symbol_count;
 
-    Section *first_section;
-    Section *last_section;
+    Elf_Section *first_section;
+    Elf_Section *last_section;
     U64 section_count;
 };
 
-internal Symbol *create_symbol(Arena *arena, Builder *builder, Str8 name) {
-    Symbol *symbol = arena_push_struct(arena, Symbol);
+internal Elf_Symbol *elf_create_symbol(Arena *arena, Elf_Object *object, Str8 name) {
+    Elf_Symbol *symbol = arena_push_struct(arena, Elf_Symbol);
     symbol->name = name;
 
-    dll_push_back(builder->first_symbol, builder->last_symbol, symbol);
-    ++builder->symbol_count;
+    dll_push_back(object->first_symbol, object->last_symbol, symbol);
+    ++object->symbol_count;
 
     return symbol;
 }
 
-internal Section *create_section(Arena *arena, Builder *builder, Str8 name) {
-    Section *section = arena_push_struct(arena, Section);
+internal Elf_Section *elf_create_section(Arena *arena, Elf_Object *object, Str8 name, Elf_SectionHeaderType type) {
+    Elf_Section *section = arena_push_struct(arena, Elf_Section);
     section->name = name;
+    section->type = type;
 
-    dll_push_back(builder->first_section, builder->last_section, section);
-    ++builder->section_count;
+    dll_push_back(object->first_section, object->last_section, section);
+    ++object->section_count;
 
     return section;
 }
 
-internal U64 section_index_from_name(Builder *builder, Str8 name) {
+internal U64 elf_section_index_from_name(Elf_Object *object, Str8 name) {
     U64 result = 0;
 
     U64 index = 1;
-    for (Section *section = builder->first_section; section; section = section->next, ++index) {
+    for (Elf_Section *section = object->first_section; section; section = section->next, ++index) {
         if (str8_equal(section->name, name)) {
             result = index;
             break;
@@ -210,15 +240,15 @@ internal S32 os_run(Str8List arguments) {
 
 
 
-    Builder builder = { 0 };
+    Elf_Object object = { 0 };
 
     // NOTE(simon): Symbols
-    Symbol *size_symbol = create_symbol(arena, &builder, str8_literal("test_size"));
+    Elf_Symbol *size_symbol = elf_create_symbol(arena, &object, str8_literal("test_size"));
     size_symbol->section_name = str8_literal(".data");
     size_symbol->data         = str8((U8 *) &test_size, sizeof(test_size));
     size_symbol->align        = 8;
 
-    Symbol *data_symbol = create_symbol(arena, &builder, str8_literal("test_data"));
+    Elf_Symbol *data_symbol = elf_create_symbol(arena, &object, str8_literal("test_data"));
     data_symbol->section_name = str8_literal(".data");
     data_symbol->data         = str8(test_data, sizeof(test_data));
     data_symbol->align        = 1;
@@ -226,9 +256,8 @@ internal S32 os_run(Str8List arguments) {
 
 
     // NOTE(simon): Sections
-    Section *data_section = create_section(arena, &builder, str8_literal(".data"));
-    data_section->type  = ELF_SECTION_HEADER_TYPE_PROGRAM_BITS;
-    data_section->flags = ELF_SECTION_HEADER_FLAG_WRITE | ELF_SECTION_HEADER_FLAG_ALLOCATE;
+    Elf_Section *data_section = elf_create_section(arena, &object, str8_literal(".data"), Elf_SectionHeaderType_ProgramBits);
+    data_section->flags = Elf_SectionHeaderFlag_Write | Elf_SectionHeaderFlag_Allocate;
 
 
 
@@ -236,9 +265,9 @@ internal S32 os_run(Str8List arguments) {
 
 
     // NOTE(simon): Layout symbols.
-    for (Section *section = builder.first_section; section; section = section->next) {
+    for (Elf_Section *section = object.first_section; section; section = section->next) {
         U64 offset = 0;
-        for (Symbol *symbol = builder.first_symbol; symbol; symbol = symbol->next) {
+        for (Elf_Symbol *symbol = object.first_symbol; symbol; symbol = symbol->next) {
             if (str8_equal(section->name, symbol->section_name)) {
                 U64 aligned_offset = u64_round_up_to_power_of_2(offset, symbol->align);
 
@@ -261,17 +290,16 @@ internal S32 os_run(Str8List arguments) {
 
 
     // NOTE(simon): Create symbol string table.
-    Section *string_table_section = create_section(arena, &builder, str8_literal(".strtab"));
-    string_table_section->type          = ELF_SECTION_HEADER_TYPE_STRING_TABLE;
+    Elf_Section *string_table_section = elf_create_section(arena, &object, str8_literal(".strtab"), Elf_SectionHeaderType_StringTable);
     string_table_section->address_align = 1;
 
     U32 symbol_name_buffer_size = 1;
-    for (Symbol *symbol = builder.first_symbol; symbol; symbol = symbol->next) {
+    for (Elf_Symbol *symbol = object.first_symbol; symbol; symbol = symbol->next) {
         symbol->name_index = symbol_name_buffer_size;
         symbol_name_buffer_size += symbol->name.size + 1;
     }
     U8 *symbol_name_buffer = arena_push_array(arena, U8, symbol_name_buffer_size);
-    for (Symbol *symbol = builder.first_symbol; symbol; symbol = symbol->next) {
+    for (Elf_Symbol *symbol = object.first_symbol; symbol; symbol = symbol->next) {
         memory_copy(&symbol_name_buffer[symbol->name_index], symbol->name.data, symbol->name.size);
     }
     str8_list_push(arena, &string_table_section->data, str8(symbol_name_buffer, symbol_name_buffer_size));
@@ -279,23 +307,22 @@ internal S32 os_run(Str8List arguments) {
 
 
     // NOTE(simon): Create symbols.
-    U64 elf_symbol_count = 1 + builder.symbol_count;
+    U64 elf_symbol_count = 1 + object.symbol_count;
     Elf64_Symbol *elf_symbols = arena_push_array(arena, Elf64_Symbol, elf_symbol_count);
     U64 elf_symbol_index = 1;
-    for (Symbol *symbol = builder.first_symbol; symbol; symbol = symbol->next) {
+    for (Elf_Symbol *symbol = object.first_symbol; symbol; symbol = symbol->next) {
         Elf64_Symbol *elf_symbol = &elf_symbols[elf_symbol_index];
         elf_symbol->name          = symbol->name_index;
-        elf_symbol->info          = ELF_SYMBOL_INFO_FROM_BINDING_TYPE(ELF_SYMBOL_BINDING_GLOBAL, ELF_SYMBOL_TYPE_OBJECT);
-        elf_symbol->other         = ELF_SYMBOL_VISIBILITY_DEFAULT;
-        elf_symbol->section_index = (U16) section_index_from_name(&builder, symbol->section_name);;
+        elf_symbol->info          = ELF_SYMBOL_INFO_FROM_BINDING_TYPE(Elf_SymbolBinding_Global, Elf_SymbolType_Object);
+        elf_symbol->other         = Elf_SymbolVisibility_Default;
+        elf_symbol->section_index = (U16) elf_section_index_from_name(&object, symbol->section_name);;
         elf_symbol->value         = symbol->offset;
         elf_symbol->size          = symbol->data.size;
 
         ++elf_symbol_index;
     }
 
-    Section *symbol_table_section = create_section(arena, &builder, str8_literal(".symtab"));
-    symbol_table_section->type          = ELF_SECTION_HEADER_TYPE_SYMBOL_TABLE;
+    Elf_Section *symbol_table_section = elf_create_section(arena, &object, str8_literal(".symtab"), Elf_SectionHeaderType_SymbolTable);
     symbol_table_section->link_name     = str8_literal(".strtab");
     symbol_table_section->info          = 1; // Last local symbol index + 1
     symbol_table_section->address_align = _Alignof(Elf64_Symbol);
@@ -306,17 +333,16 @@ internal S32 os_run(Str8List arguments) {
 
 
     // NOTE(simon): Compute section string table.
-    Section *section_header_string_table_section = create_section(arena, &builder, str8_literal(".shstrtab"));
-    section_header_string_table_section->type          = ELF_SECTION_HEADER_TYPE_STRING_TABLE;
+    Elf_Section *section_header_string_table_section = elf_create_section(arena, &object, str8_literal(".shstrtab"), Elf_SectionHeaderType_StringTable);
     section_header_string_table_section->address_align = 1;
 
     U32 section_name_buffer_size = 1;
-    for (Section *section = builder.first_section; section; section = section->next) {
+    for (Elf_Section *section = object.first_section; section; section = section->next) {
         section->name_index = section_name_buffer_size;
         section_name_buffer_size += section->name.size + 1;
     }
     U8 *section_name_buffer = arena_push_array(arena, U8, section_name_buffer_size);
-    for (Section *section = builder.first_section; section; section = section->next) {
+    for (Elf_Section *section = object.first_section; section; section = section->next) {
         memory_copy(&section_name_buffer[section->name_index], section->name.data, section->name.size);
     }
     str8_list_push(arena, &section_header_string_table_section->data, str8(section_name_buffer, section_name_buffer_size));
@@ -325,8 +351,8 @@ internal S32 os_run(Str8List arguments) {
 
     // NOTE(simon): Layout section content.
     {
-        U64 total_offset = sizeof(Elf64_Header) + (1 + builder.section_count) * sizeof(Elf64_SectionHeader);
-        for (Section *section = builder.first_section; section; section = section->next) {
+        U64 total_offset = sizeof(Elf64_Header) + (1 + object.section_count) * sizeof(Elf64_SectionHeader);
+        for (Elf_Section *section = object.first_section; section; section = section->next) {
             section->offset = total_offset;
             total_offset += section->data.total_size;
         }
@@ -335,10 +361,10 @@ internal S32 os_run(Str8List arguments) {
 
 
     // NOTE(simon): Create sections.
-    U64 elf_section_count = 1 + builder.section_count;
+    U64 elf_section_count = 1 + object.section_count;
     Elf64_SectionHeader *elf_sections = arena_push_array(arena, Elf64_SectionHeader, elf_section_count);
     U64 elf_section_index = 1;
-    for (Section *section = builder.first_section; section; section = section->next) {
+    for (Elf_Section *section = object.first_section; section; section = section->next) {
         Elf64_SectionHeader *elf_section = &elf_sections[elf_section_index];
         elf_section->name          = section->name_index;
         elf_section->type          = section->type;
@@ -346,7 +372,7 @@ internal S32 os_run(Str8List arguments) {
         elf_section->address       = section->address;
         elf_section->offset        = section->offset;
         elf_section->size          = section->data.total_size;
-        elf_section->link          = (U32) section_index_from_name(&builder, section->link_name);
+        elf_section->link          = (U32) elf_section_index_from_name(&object, section->link_name);
         elf_section->info          = section->info;
         elf_section->address_align = section->address_align;
         elf_section->entry_size    = section->entry_size;
@@ -362,19 +388,19 @@ internal S32 os_run(Str8List arguments) {
     header.identification[Elf_HeaderIdentification_Magic1]     = 'E';
     header.identification[Elf_HeaderIdentification_Magic2]     = 'L';
     header.identification[Elf_HeaderIdentification_Magic3]     = 'F';
-    header.identification[Elf_HeaderIdentification_Class]      = ELF_HEADER_CLASS_64;
-    header.identification[Elf_HeaderIdentification_Data]       = ELF_HEADER_DATA_2LSB;
-    header.identification[Elf_HeaderIdentification_Version]    = ELF_HEADER_VERSION_CURRENT;
-    header.identification[Elf_HeaderIdentification_OsAbi]      = ELF_OS_ABI_NONE;
+    header.identification[Elf_HeaderIdentification_Class]      = Elf_HeaderClass_64;
+    header.identification[Elf_HeaderIdentification_Data]       = Elf_HeaderData_2Lsb;
+    header.identification[Elf_HeaderIdentification_Version]    = Elf_HeaderVersion_Current;
+    header.identification[Elf_HeaderIdentification_OsAbi]      = Elf_OsAbi_None;
     header.identification[Elf_HeaderIdentification_AbiVersion] = 0;
-    header.type                        = ELF_HEADER_TYPE_RELOCATABLE;
-    header.machine                     = ELF_HEADER_MACHINE_X86_64;
-    header.version                     = ELF_HEADER_VERSION_CURRENT;
+    header.type                        = Elf_HeaderType_Relocatable;
+    header.machine                     = Elf_HeaderMachine_X86_64;
+    header.version                     = Elf_HeaderVersion_Current;
     header.section_header_offset       = sizeof(Elf64_Header);
     header.header_size                 = sizeof(Elf64_Header);
     header.section_header_entry_size   = sizeof(Elf64_SectionHeader);
     header.section_header_count        = (U16) elf_section_count;
-    header.section_header_string_index = (U16) section_index_from_name(&builder, str8_literal(".shstrtab"));
+    header.section_header_string_index = (U16) elf_section_index_from_name(&object, str8_literal(".shstrtab"));
 
 
 
@@ -382,7 +408,7 @@ internal S32 os_run(Str8List arguments) {
     Str8List output = { 0 };
     str8_list_push(arena, &output, str8((U8 *) &header, sizeof(header)));
     str8_list_push(arena, &output, str8((U8 *) elf_sections, elf_section_count * sizeof(Elf64_SectionHeader)));
-    for (Section *section = builder.first_section; section; section = section->next) {
+    for (Elf_Section *section = object.first_section; section; section = section->next) {
         for (Str8Node *data = section->data.first; data; data = data->next) {
             str8_list_push(arena, &output, data->string);
         }
