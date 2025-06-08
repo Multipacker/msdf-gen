4f93c8ff5b39ef828afbdccc3d16455c536ef37d
Simon Renhult <simon.renhult@hotmail.se>
2025-06-08 18:39:40 +0200

    Dev: Remove generated files from repo as they are regenerated on every build

--- src/render/opengl/generated.h
+++
@@ -1,6 +0,0 @@
-#undef embed_file
-#define embed_file(...)
-extern U8 opengl_vertex_shader_data[2864];
-global Str8 opengl_vertex_shader = (Str8) { opengl_vertex_shader_data, 2864, };
-extern U8 opengl_fragment_shader_data[3358];
-global Str8 opengl_fragment_shader = (Str8) { opengl_fragment_shader_data, 3358, };
