ab9fa60a4246941e378962689d15dcf387a4716e
Simon Renhult <simon.renhult@hotmail.se>
2025-06-10 21:22:23 +0200

    Dev(base): Integrate metaprogram into MSVC build script

--- scripts/build_msvc.bat
+++ scripts/build_msvc.bat
@@ -4,22 +4,22 @@ setlocal
 for %%a in (%*) do set "%%a=1"
 if not "%release%" == "1" if not "%profile%" == "1" set debug=1
 
-rem 4201: nonstandard extension used: nameless struct/union
-rem 4152: nonstandard extension used: function/data ptr conversion in expression
-rem 4100: unreferenced formal parameter
-rem 4189: local variable is initialized but not referenced
-rem 4101: unreferenced local variable
-rem 4310: cast truncates constant value
-rem 4061: enum case not explicitly handled
-rem 4820: n bytes padding added after data member x
-rem 5045: Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
-rem 4711: x selected for automatic inline expansion
-rem 4710: function not inlined
-rem 4242: conversion from U32 to U16
-rem 4244: conversion from U32 to U8
-rem 4668: undefined preprocessor macros
+:: 4201: nonstandard extension used: nameless struct/union
+:: 4152: nonstandard extension used: function/data ptr conversion in expression
+:: 4100: unreferenced formal parameter
+:: 4189: local variable is initialized but not referenced
+:: 4101: unreferenced local variable
+:: 4310: cast truncates constant value
+:: 4061: enum case not explicitly handled
+:: 4820: n bytes padding added after data member x
+:: 5045: Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
+:: 4711: x selected for automatic inline expansion
+:: 4710: function not inlined
+:: 4242: conversion from U32 to U16
+:: 4244: conversion from U32 to U8
+:: 4668: undefined preprocessor macros
 
-rem Common flags
+:: Common flags
 
 set disabled_warnings=-wd4201 -wd4152 -wd4100 -wd4189 -wd4101 -wd4310 -wd4061 -wd4820 -wd4191 -wd5045 -wd4711 -wd4710 -wd4242 -wd4244 -wd4668
 
@@ -27,15 +27,17 @@ set libs=User32.lib Opengl32.lib Gdi32.lib Shell32.lib Bcrypt.lib
 set common_compiler_flags=-nologo -FC -Wall -MP -WX %disabled_warnings% -I.. -diagnostics:caret -diagnostics:color
 set common_linker_flags=%libs% -incremental:no
 
-rem Debug flags
+:: Debug flags
 set debug_compiler_flags=%common_compiler_flags% -RTC1 -MTd -Zi -DCONSOLE=1 -DENABLE_ASSERT=1 -DDEBUG_BUILD=1
 set debug_linker_flags=%common_linker_flags%
 
-rem Release flags
+:: Release flags
 set release_compiler_flags=%common_compiler_flags% -MT -O2 -Oi -EHsc -GS- -DRELEASE_BUILD=1
 set release_linker_flags=%common_linker_flags% -fixed -opt:icf -opt:ref -subsystem:windows libvcruntime.lib
 
-rem Choose options
+set render_libraries=opengl.o
+
+:: Choose options
 if "%debug%" == "1" (
     echo Debug build
     set compiler_flags=%debug_compiler_flags%
@@ -53,8 +55,13 @@ if "%profile%" == "1" (
 )
 
 if not exist build mkdir build
+
+:: Build and run metaprogram
 pushd build
+cl ../src/meta/main.c %compiler_flags% -link %linker_flags% -out:"meta.exe" || exit /b 1
+popd
+build\meta || exit /b 1
 
-cl ../src/msdf-gen/main.c %compiler_flags% -link %linker_flags% -out:"msdf-gen.exe"
-
+pushd build
+cl ../src/msdf-gen/main.c %compiler_flags% -link %linker_flags% %render_libraries% msdf-gen.o -out:"msdf-gen.exe" || exit /b 1
 popd
